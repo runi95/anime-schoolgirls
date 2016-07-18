7ede2234440a85eb30f20350f9de43a0d8e0fb25
Shadow <Shadow@DESKTOP-EUG89AS>
2016-07-18 22:27:30 +0200

    Try this

--- anime-schoolgirls/src/api/JsonDecoder.java
+++ anime-schoolgirls/src/api/JsonDecoder.java
@@ -8,7 +8,10 @@ public class JsonDecoder {
 	public static void handleJson(String ftwJson) throws Exception{
 		Gson gson = new Gson();
 		//String testline = "{'status':'200','message':'7231690b-2f3c-4c35-b9e6-4aa48ae84ddd'}"; 
-		gson.fromJson(ftwJson, userInfo.class);
+		userInfo info = gson.fromJson(ftwJson, userInfo.class);
+		// Like this, right? ^
+		// And then retrieve info like...
+		System.out.println(info.getUserToken()); //?
 	}
 	
 	public class userInfo
