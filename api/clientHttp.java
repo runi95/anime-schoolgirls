12139ebfefc1b58e7d8e0ff19efd65efd088bd39
henning <henning@localhost.localdomain>
2016-07-18 20:34:16 +0200

    pushing simple api interaction

---
+++ api/clientHttp.java
@@ -0,0 +1,86 @@
+package api;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import api.JsonDecoder.userInfo;
+
+
+
+
+@SuppressWarnings("deprecation")
+public class clientHttp {
+
+	private final String USER_AGENT = "HenningCast/javaCast";
+	private final String DEV_KEY = "qruE-204Y-ZXgr-GLRZ";
+	private final String BASE_URL = "https://www.animeftw.tv/api/v2/";
+	private final String USER_NAME = "";
+	private final String USER_PASS = "";
+	
+
+	public static void main(String[] args) throws Exception {
+
+		clientHttp http = new clientHttp();
+
+
+		System.out.println("\nTesting 2 - Send Http POST request");
+		http.sendLoginRequest();
+		
+
+	}
+
+
+	// HTTP POST request
+	private void sendLoginRequest() throws Exception {
+		
+		String url = BASE_URL;
+
+		HttpClient client = new DefaultHttpClient();
+		HttpPost post = new HttpPost(url);
+
+		// add header
+		post.setHeader("User-Agent", USER_AGENT);
+
+		List<NameValuePair> urlParameters = new ArrayList<NameValuePair>();
+		urlParameters.add(new BasicNameValuePair("devkey", DEV_KEY));
+		urlParameters.add(new BasicNameValuePair("username", USER_NAME));
+		urlParameters.add(new BasicNameValuePair("password", USER_PASS));
+//		urlParameters.add(new BasicNameValuePair("caller", ""));
+//		urlParameters.add(new BasicNameValuePair("num", "12345"));
+
+		post.setEntity(new UrlEncodedFormEntity(urlParameters));
+
+		HttpResponse response = client.execute(post);
+		System.out.println("\nSending 'POST' request to URL : " + url);
+		System.out.println("Post parameters : " + post.getEntity());
+		System.out.println("Response Code : " + 
+                                    response.getStatusLine().getStatusCode());
+
+		BufferedReader rd = new BufferedReader(
+                        new InputStreamReader(response.getEntity().getContent()));
+
+		StringBuffer result = new StringBuffer();
+		String line = "";
+		while ((line = rd.readLine()) != null) {
+			result.append(line);
+		}
+
+		//System.out.println();
+		JsonDecoder.handleJson(result.toString());
+
+	}
+	
+
+	
+}
+
