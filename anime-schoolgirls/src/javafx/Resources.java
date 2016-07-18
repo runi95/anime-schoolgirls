bc87c48a33ffcdaa77a38957053304d05d03d63f
Shadow <Shadow@DESKTOP-EUG89AS>
2016-07-18 19:20:41 +0200

    Commit

---
+++ anime-schoolgirls/src/javafx/Resources.java
@@ -0,0 +1,29 @@
+package javafx;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+
+public class Resources {
+		public static void loadFXML(Node node) {
+			try {
+				String name = "/server/ui/resources/fxml/" + node.getClass().getSimpleName() + ".fxml";
+				FXMLLoader fxmlLoader = new FXMLLoader(Resources.class.getResource(name));
+
+				fxmlLoader.setRoot(node);
+				fxmlLoader.setController(node);
+
+				System.out.println("Loading: " + node.getClass().getSimpleName());
+
+				fxmlLoader.load();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public static void main(String[] args) {
+			Application.launch(MyApplication.class, args);
+		}
+}
